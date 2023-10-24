0fa15c9e7886f49249229aa22b9be715d1a51fe6
Alfred Tingson <alfred.tingson@stratpoint.com>
2023-10-24 17:10:45 +0800

    feat: integrated sendGrid to send alerts about thresholds

--- routes/sensorRoutes.js
+++ routes/sensorRoutes.js
@@ -2,6 +2,9 @@ const express = require('express');
 const Sensor = require('../models/sensor')
 const { validateId, sensorValidationRules, validateRules } = require('../middleware/validatorMiddleware');
 const router = express.Router();
+const sgMail = require('@sendgrid/mail')
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 
 // get all sensor data
 router.get('/', async (req, res, next) => {
@@ -28,9 +31,66 @@ router.get('/:id', validateId, validateRules, async (req, res, next) => {
 
 // create sensor data
 router.post('/', sensorValidationRules, validateRules, async (req, res, next) => {
+    const { location, temperature_celsius, humidity_percent, pressure_hpa } = req.body;
+
     try {
         const sensor = new Sensor(req.body);
         const createdSensorData = await sensor.save();
+        // Check temperature threshold
+        if (temperature_celsius > 30) {
+            console.log(`High temperature detected at ${location}: ${temperature_celsius}°C (id: ${sensor._id})`);
+            const msg = {
+                to: `${process.env.EMAIL_TO}`,
+                from: `${process.env.EMAIL_FROM}`,
+                subject: 'High Temperature Alert',
+                text: `High temperature detected at ${location}: ${temperature_celsius}°C (id: ${sensor._id})`,
+            };
+            sgMail.send(msg)
+                .then(() => {
+                    console.log('Email alert sent');
+                })
+                .catch((error) => {
+                    console.error('Error sending email alert:', error);
+                });
+
+        }
+
+        // Check humidity threshold
+        if (humidity_percent > 70) {
+            console.log(`High Humidity Percentage detected at ${location}: ${humidity_percent}% (id: ${sensor._id})`);
+            const msg = {
+                to: `${process.env.EMAIL_TO}`,
+                from: `${process.env.EMAIL_FROM}`,
+                subject: 'High Humidity Percentage Alert',
+                text: `High Humidity Percentage detected at ${location}: ${humidity_percent}% (id: ${sensor._id})`,
+            };
+            sgMail.send(msg)
+                .then(() => {
+                    console.log('Email alert sent');
+                })
+                .catch((error) => {
+                    console.error('Error sending email alert:', error);
+                });
+
+        }
+
+        // Check pressure threshold
+        if (pressure_hpa > 1000) {
+            console.log(`High Pressure detected at ${location}: ${pressure_hpa} hPa (id: ${sensor._id})`);
+            const msg = {
+                to: `${process.env.EMAIL_TO}`,
+                from: `${process.env.EMAIL_FROM}`,
+                subject: 'High Pressure Alert',
+                text: `High Pressure detected at ${location}: ${pressure_hpa} hPa (id: ${sensor._id})`,
+            };
+            sgMail.send(msg)
+                .then(() => {
+                    console.log('Email alert sent');
+                })
+                .catch((error) => {
+                    console.error('Error sending email alert:', error);
+                });
+        }
         res.status(201).json(createdSensorData);
     } catch (error) {
         next(error);
